1b3ac0431b12f446b9cf5cd53634da9acadb142d
Ricardo2708 <vegaricardo636@gmail.com>
2024-03-21 17:12:48 -0600

    bugfix register.py

--- project/register.py
+++ project/register.py
@@ -3,7 +3,7 @@ from flask_admin.contrib.sqla import ModelView
 from flask_login import current_user
 from flask import redirect, url_for, request, flash
 from flask_admin import BaseView, expose
-from sqlalchemy import desc
+from sqlalchemy import desc, inspect
 
 def to_camel_case(text):
     return ''.join(word.title() for word in text.split('_'))
@@ -20,11 +20,12 @@ class MicroBlogModelView(ModelView):
     def index(self):
         path = request.path.strip('/').split('/')[1]
         class_model = globals().get(to_camel_case(path))
-        model = class_model.query.order_by(class_model.id.desc()).all()
-        return self.render('admin/private/dynamic/index.html', path = to_camel_case(path), models = model )
+        primary_key_name = inspect(class_model).primary_key[0].name
+        model = class_model.query.order_by(getattr(class_model, primary_key_name).desc()).all()
+        return self.render('admin/private/dynamic/index.html', path = path, models = model )
 
 def register_admin_views(admin):
-    models_to_register = []
+    models_to_register = [Clientes]
     for model in models_to_register:
         admin.add_view(MicroBlogModelView(model, models.session))
     return [model.__name__.lower() for model in models_to_register]
